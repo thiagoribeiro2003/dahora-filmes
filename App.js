b47ef1b4ba0b3fc2572b85ec0756ee1ff45af264
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-11-07 09:17:10 -0300

    Criando views
    Criação das views container, botoes, rodape, e estilização das mesmas

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const App = () => {
   return (
-    <View style={estilos.viewLogo}>
-      <Text>App</Text>
-    </View>
+    <SafeAreaView style={estilos.container}>
+      <View style={estilos.viewLogo}>
+        <Text>Dá hora filmes</Text>
+      </View>
+
+      <View style={estilos.viewBotoes}>
+        <Button title="Buscar Filmes" />
+        <Button title="Favoritos" />
+      </View>
+
+      <View style={estilos.viewRodape}>
+        <Button title="Privacidade" />
+        <Button title="Sobre" />
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -19,7 +31,20 @@ const estilos = StyleSheet.create({
     justifyContent: "center",
   },
   viewLogo: {
+    width: "80%",
     backgroundColor: "green",
     textAlign: "center",
+  },
+  viewBotoes: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    backgroundColor: "orange",
+    width: "80%",
+  },
+  viewRodape: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "red",
+    width: "80%",
   },
 });
