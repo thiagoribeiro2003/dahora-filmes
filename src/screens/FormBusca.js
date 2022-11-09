ae7287db1fa03740a3f2b4e7e9a7d9d03d073000
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-11-09 08:57:43 -0300

    Iniciando exercicio

--- src/screens/FormBusca.js
+++ src/screens/FormBusca.js
@@ -4,11 +4,27 @@ import React from "react";
 const FormBusca = () => {
   return (
     <SafeAreaView>
-      <Text>FormBusca</Text>
+      <Text style={estilos.textoBusca1}>
+        Aventura? Terror? Suspense? Ação?{" "}
+      </Text>
+      <Text style={estilos.textoBusca2}>
+        Localize um filme que você viu ou gostaria de ver!
+      </Text>
     </SafeAreaView>
   );
 };
 
 export default FormBusca;
 
-const styles = StyleSheet.create({});
+const estilos = StyleSheet.create({
+  textoBusca1: {
+    marginTop: 30,
+    margin: 15,
+    textAlign: "left",
+  },
+  textoBusca2: {
+    margin: 15,
+    marginTop: 10,
+    textAlign: "left",
+  },
+});
