0a175ad56459fbf780d51c8a91370517abf6641a
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-11-10 11:43:45 -0300

    Trazendo resultados da API, quando pesquisa

--- src/screens/Resultados.js
+++ src/screens/Resultados.js
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import api from "../services/api";
 
 const Resultados = ({ route }) => {
   /* Usamos a proproute (do React Navigation) para
   acessar os parâmetros desta rota de navegação e extrair
   os dados (neste casom filme) enviados para esta tela Resultados */
   const { filme } = route.params;
+
+  const [resultados, setResultados] = useState([]);
+
+  useEffect(() => {
+    async function buscarFilmes() {
+      try {
+        const resposta = await api.get("/search/movie", {
+          params: {
+            api_key: "0bf24795fa0f8c3e3cef5a15e6e002a2",
+            language: "pt-BR",
+            query: filme,
+            include_adult: false,
+          },
+        });
+
+        setResultados(resposta.data);
+      } catch (error) {
+        console.log("Deu ruim na busca da API: " + error.message);
+      }
+    }
+    buscarFilmes();
+  }, []);
+
+  console.log(resultados);
 
   return (
     <SafeAreaView style={estilos.container}>
