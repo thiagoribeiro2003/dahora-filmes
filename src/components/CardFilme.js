efed2351a341f533738b1814fd623951318dc4e2
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-11-17 10:40:23 -0300

    Programação de salvar Favoritos no Storage

--- src/components/CardFilme.js
+++ src/components/CardFilme.js
@@ -1,3 +1,5 @@
+// importa o AsyncStorage do expo, Não use do react-native padrão
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StyleSheet, Text, View, Image, Pressable, Alert } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
@@ -13,6 +15,32 @@ const CardFilme = ({ filme }) => {
     /* Acrecentar no App na parte de Stack.Screen a página que deseja*/
     navigation.navigate("Detalhes", { filme });
   };
+
+  const salvar = async () => {
+    // return Alert.alert("Favoritos", "Salvando");
+
+    /* Etapas para uso do AsyncStorage */
+
+    // 1) Carregamento do storage do aparelho (se houver, caso contrário retorna null)
+    const filmesFavoritos = await AsyncStorage.getItem("@favoritos");
+    console.log(filmesFavoritos);
+
+    // 2) Havendo storage prévio, transformamos os doados do filme em um objeto e os guardamos nume lista (array)
+    let listaDeFilmes = JSON.parse(filmesFavoritos);
+
+    // 3) Se a lista não for indefinida, vamos iniciá-la como um array vazio
+    if (!listaDeFilmes) {
+      listaDeFilmes = [];
+    }
+
+    // 4) Adicionamos os dados do filme na lista (array)
+    listaDeFilmes.push(filme);
+
+    // 5) Finalmente, salvamos no storage do dispositivo
+    await AsyncStorage.setItem("@favoritos", JSON.stringify(listaDeFilmes));
+    Alert.alert("Favotiros", "Filme salvo com sucesso!");
+  };
+
   const { title, poster_path } = filme;
   return (
     <View style={estilos.card}>
@@ -36,7 +64,7 @@ const CardFilme = ({ filme }) => {
               <Ionicons name="book" size={12} /> Leia mais
             </Text>
           </Pressable>
-          <Pressable style={estilos.botao}>
+          <Pressable style={estilos.botao} onPress={salvar}>
             <Text style={estilos.textoBotao}>
               <Ionicons name="add-circle" size={12} /> Salvar
             </Text>
